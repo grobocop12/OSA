a3e3a02d34f71a0868c498994045e225d71db88c
grobocop <chasnik97876@gmail.com>
2018-10-31 18:10:55 +0100

    python files modification

--- pliki pythona/wav_file_reader.py
+++ pliki pythona/wav_file_reader.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt, mpld3
 from scipy import signal
 from scipy.io import wavfile
 import scipy
@@ -9,24 +9,34 @@ import numpy as np
 time = 1
 
 sample_rate, samples = wavfile.read('Bit - Hacknet OST - 08 You Got Mail.wav')
-#sample_rate, samples = wavfile.read('Perturbator.wav')
+
+
+'''
 normalized_samples = np.ndarray(shape= sample_rate*time)
-#for i in range(sample_rate*time):
-#    normalized_samples[i] =( (samples[i][0]+samples[i][1])/2)
+for i in range(sample_rate*time):
+    normalized_samples[i] =( (samples[i][0]+samples[i][1])/2)
+'''
+
 
 frequencies, times, spectrogram = signal.spectrogram(samples,
                                                      fs = sample_rate,
-                                                     window = signal.hamming(256),
+                                                     window = signal.hamming(1024),
                                                      scaling = 'spectrum')
+'''
+frequencies, times, spectrogram = signal.stft(normalized_samples[0:1000], fs = sample_rate, nperseg = 1000)
+print(times)
 
-#frequencies, times, spectrogram = signal.stft(normalized_samples[0:1000], fs = sample_rate, nperseg = 1000)
-#print(times)
+ifft = scipy.fftpack.ifft(samples[0:44101])
+'''
+print('times',times.shape)
+print('frequencies',frequencies.shape)
+print('spectrogram',spectrogram.shape)
 
-#ifft = scipy.fftpack.ifft(samples[0:44101])
 
 plt.pcolormesh(times, frequencies, spectrogram)
 plt.ylabel('Frequency [Hz]')
 plt.xlabel('Time [sec]')
+plt.tight_layout()
 plt.show()
 
 '''
