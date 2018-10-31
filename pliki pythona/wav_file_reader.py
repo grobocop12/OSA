c0ae87152ef6b419f4fcdf396fa6dc393395a032
grobocop <chasnik97876@gmail.com>
2018-10-31 18:22:51 +0100

    merge analiza_dzwieku

--- pliki pythona/wav_file_reader.py
+++ pliki pythona/wav_file_reader.py
@@ -11,23 +11,13 @@ time = 1
 sample_rate, samples = wavfile.read('Bit - Hacknet OST - 08 You Got Mail.wav')
 
 
-'''
-normalized_samples = np.ndarray(shape= sample_rate*time)
-for i in range(sample_rate*time):
-    normalized_samples[i] =( (samples[i][0]+samples[i][1])/2)
-'''
 
 
 frequencies, times, spectrogram = signal.spectrogram(samples,
                                                      fs = sample_rate,
                                                      window = signal.hamming(1024),
                                                      scaling = 'spectrum')
-'''
-frequencies, times, spectrogram = signal.stft(normalized_samples[0:1000], fs = sample_rate, nperseg = 1000)
-print(times)
 
-ifft = scipy.fftpack.ifft(samples[0:44101])
-'''
 print('times',times.shape)
 print('frequencies',frequencies.shape)
 print('spectrogram',spectrogram.shape)
@@ -39,8 +29,4 @@ plt.xlabel('Time [sec]')
 plt.tight_layout()
 plt.show()
 
-'''
-plt.plot(np.abs(spectrogram))
-plt.show()
-
-'''
+S
