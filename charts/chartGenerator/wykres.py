23139750c121f43f7abc897f01e63c4320f155ce
grobocop <chasnik97876@gmail.com>
2018-11-04 01:45:58 +0100

    mod pod nasze dane

--- charts/chartGenerator/wykres.py
+++ charts/chartGenerator/wykres.py
@@ -31,7 +31,7 @@ def plot_hist():
     fig.set_figwidth(6.4)
     fig.set_figheight(3.2)
     plt.hist(samples,density=10,color='g')
-    plt.title("To jest kurwa jebany w dupe histogram")
+    plt.title("To jest !@#$% ^&*{} w ~@!# histogram")
     plt.grid()
     plt.tight_layout()
     #plt.axis('off')
@@ -40,26 +40,13 @@ def plot_hist():
 
 
 def plot_spect():
-    numpy.random.seed(19680801)
-    dt = 0.0005
-    t = numpy.arange(0.0, 20.0, dt)
-    s1 = numpy.sin(2 * numpy.pi * 100 * t)
-    s2 = 2 * numpy.sin(2 * numpy.pi * 400 * t)
-
-    # create a transient "chirp"
-    mask = numpy.where(numpy.logical_and(t > 10, t < 12), 1.0, 0.0)
-    s2 = s2 * mask
-
-    # add some noise into the mix
-    nse = 0.01 * numpy.random.random(size=len(t))
-
-    x = s1 + s2 + nse  # the signal
+    fs, samples = wavfile.read('Bit - Hacknet OST - 08 You Got Mail.wav')
     NFFT = 1024  # the length of the windowing segments
-    Fs = int(1.0 / dt)  # the sampling frequency
-
+    time = numpy.arange(0,len(samples),dtype = float)
+    time = numpy.divide(time, fs)
     fig, (ax1, ax2) = plt.subplots(nrows=2)
-    ax1.plot(t, x)
-    Pxx, freqs, bins, im = ax2.specgram(x, NFFT=NFFT, Fs=Fs, noverlap=900)
+    ax1.plot(time, samples)
+    Pxx, freqs, bins, im = ax2.specgram(samples, NFFT=NFFT, Fs=fs, noverlap=0)
     # The `specgram` method returns 4 objects. They are:
     # - Pxx: the periodogram
     # - freqs: the frequency vector
