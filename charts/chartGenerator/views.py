770cef311f77cd5c7a37b83d086d42d51b6f9c66
grobocop12 <chasnik97876@gmail.com>
2019-01-14 00:15:14 +0100

    get request

--- charts/chartGenerator/views.py
+++ charts/chartGenerator/views.py
@@ -10,6 +10,9 @@ import json
 from .forms import UploadFileForm
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.views.generic import TemplateView
+import numpy as np
+import scipy.io.wavfile as wavfile
+import io
 
 
 def index(request):
@@ -48,6 +51,9 @@ def upload_file(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             sample_rate, samples = wykres.handle_uploaded_file(request.FILES['file'])
+            request.session['sample_rate'] = sample_rate
+            request.session['samples'] = samples.tolist()
+            
             return poligon(request,sample_rate,samples)
     else:
         form = UploadFileForm()
@@ -55,19 +61,34 @@ def upload_file(request):
 
 @csrf_exempt
 def download_file(request):
-    
-    data = request.POST.items
-    print(data)
+    if request.method == 'GET':
+        #print('GET REQUEST!!!')
+        start = float(request.GET.get('start'))
+        stop = float(request.GET.get('stop'))
+        sample_rate = request.session['sample_rate']
+        samples = np.asanyarray(request.session['samples'])
+        T = len(samples)/sample_rate
+        print(T)
+        dT = T/len(samples)
+        
+        #time = np.asanyarray(request.session['time'])
+        time = np.arange(0,len(samples),dtype = float)
+        time = np.multiply(time,dT)
+        
+        first = np.where(time>start)[0][0]
+        last = np.where(time<stop)[-1][-1]
+        samples = samples[first:last]
+        print(samples)
+        wavfile.write('tempfiles/temp.wav', sample_rate, samples)        
+        
         
     return render(request, 'chartGenerator/download.html')
     
 
 def poligon(request,sample_rate,samples):
     size = (16, 8)
-
-
-
     signal, time = wykres.poligon(sample_rate,samples)
+    #request.session['time'] = time.tolist()
     spect, frequencies ,times = wykres.spectimg(sample_rate,samples)
     return render(request,'chartGenerator/poligon.html',{'rawData':json.dumps(samples.tolist()),'time':json.dumps(time.tolist()),'signal':json.dumps(signal.tolist()),'spect':json.dumps(spect.tolist()),'fq':json.dumps(frequencies.tolist()),'t':json.dumps(times.tolist())})
     
